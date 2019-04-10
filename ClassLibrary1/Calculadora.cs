db5d9b19d29723ac6fdba3d2b50549408d73469b
Federico Perez <fede._perez@hotmail.com>
2019-04-10 20:49:02 -0300

    Add project files.

---
+++ ClassLibrary1/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassLibrary1 {
+    //La clase tiene que ser publica, y una vez referenciada se puede usar
+    public class Calculadora {
+        //Sumar method
+        public static int Sumar(string num1, string num2) {
+            int n1 = Int32.Parse(num1);
+            int n2 = Int32.Parse(num2);
+            int suma = n1 + n2;
+            return suma;
+        }
+
+        //Restar method
+        public static int Restar(string num1, string num2) {
+            int n1 = Int32.Parse(num1);
+            int n2 = Int32.Parse(num2);
+            int resta = n1 - n2;
+            return resta;
+        }
+    }
+}
---
+++ WebApplication1/Controllers/CalculadoraController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//Para agregar libreria de clases, References -> click derecho, agregar referencias de la libreria creada
+using ClassLibrary1;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: Calculadora
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Sobrecargar el metodo para mostrar primero la pagina sin datos y luego con el post a la mismapag
+        public ActionResult Suma() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Suma(string num1, string num2)
+        {
+            int res = Calculadora.Sumar(num1, num2);
+            ViewBag.SumaRes = res;
+            return View();
+        }
+
+        //funciona sin sobrecargar el metodo, validando si los parametros son nulos
+        public ActionResult Resta(string num1, string num2) {
+            if (num1 != null && num2 != null) {
+                int res = Calculadora.Restar(num1, num2);
+                ViewBag.RestaRes = res;
+            }
+            return View();
+        }
+    }
+}
