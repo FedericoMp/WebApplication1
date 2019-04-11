a384ba2674aeabce5f3889704ca5be58a53afd1b
Federico Perez <fede._perez@hotmail.com>
2019-04-10 21:51:51 -0300

    Se agrega la clase en models para tipar la vista, solo con el metodo Resta del controlador Calculadora. Update de la view Resta, agregando el @model

--- WebApplication1/Controllers/CalculadoraController.cs
+++ WebApplication1/Controllers/CalculadoraController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 
 //Para agregar libreria de clases, References -> click derecho, agregar referencias de la libreria creada
 using ClassLibrary1;
+//Siempre chequear los namespaces, se enlazan con using 
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -30,12 +32,20 @@ namespace WebApplication1.Controllers
         }
 
         //funciona sin sobrecargar el metodo, validando si los parametros son nulos
-        public ActionResult Resta(string num1, string num2) {
-            if (num1 != null && num2 != null) {
-                int res = Calculadora.Restar(num1, num2);
-                ViewBag.RestaRes = res;
-            }
-            return View();
+        //public ActionResult Resta(string num1, string num2) {
+        //    if (num1 != null && num2 != null) {
+        //        int res = Calculadora.Restar(num1, num2);
+        //        ViewBag.RestaRes = res;
+        //    }
+        //    return View();
+        //}
+
+        //Action Resta con binding automatico a traves del model
+        public ActionResult Resta(CalculadoraModel model) {
+            int R = model.Num1 + model.Num2;
+            model.Result = R;
+            //la vista es TIPADA
+            return View(model);
         }
     }
 }
